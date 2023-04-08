8962d93f8630880d0945901b4b34934aa64c7ea6
NEVIN <nevin@pop-os.localdomain>
2023-04-08 18:47:07 +0530

    Added code to capture video from the default camera, overlay it onto a background image, and display the resulting image. Also reads images from a folder and stores them in a list.

--- main.py
+++ main.py
@@ -1,10 +1,26 @@
+import os
+
 import cv2
 
 cap = cv2.VideoCapture(0)
-cap.set(3, 1280)
-cap.set(4, 720)
+cap.set(3, 640)
+cap.set(4, 480)
+
+imgBackgrund = cv2.imread('Resources/background.png')
+
+
+folderModePath = 'Resources/Modes'         # sets the path to the folder containing the images
+modePathList = os.listdir(folderModePath)    # gets a list of all the files in the folder
+imgModeList = []
+for path in modePathList:
+    imgModeList.append(cv2.imread(os.path.join(folderModePath,path)))  # reads the image at the path and appends it to the imgModeList
+print(len(imgModeList))
 
 while True:
     success, img = cap.read()
-    cv2.imshow("Face Attendance", img)
+
+    imgBackgrund[162:162+480,55:55+640] = img #adjest the image
+
+    #cv2.imshow("webcam", img)
+    cv2.imshow("Face Attendance", imgBackgrund) #to overlay webcam live strame to the background.png
     cv2.waitKey(1)
